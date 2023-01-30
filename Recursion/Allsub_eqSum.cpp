2b9a8da7922a31408066bf6fdc15d523bb525ca2
Sumedha758 <sumedhamishra64@gamil.com>
2023-01-31 03:10:46 +0530

    All_subsequeces_having_sum_equal_to_k

---
+++ Recursion/Allsub_eqSum.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printS(int ind,vector<int>&ds,int s,int sum,int arr[],int n){
+  if(ind == n){
+    if(s == sum){
+      for(auto it:ds) cout<< it << " ";
+      cout << endl;
+    }
+    return;
+  }
+  ds.push_back(arr[ind]);
+  s+=arr[ind];
+  printS(ind+1,ds,s,sum,arr,n);
+  s-=arr[ind];
+  ds.pop_back();
+  printS(ind+1,ds,s,sum,arr,n);
+}
+
+int main(){
+  int arr[]={1,2,1};
+  int n =3;
+  int sum = 2;
+  vector<int>ds;
+  printS(0,ds,0,sum,arr,n);
+  return 0;
+}
