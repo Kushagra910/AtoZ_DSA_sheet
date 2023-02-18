175d0a2d8b266291106c918d734e9dd5eca8ccdd
Sumedha758 <sumedhamishra64@gamil.com>
2023-02-18 17:46:52 +0530

    switch_case

---
+++ Basics/switch_statement.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+  int day;
+  cin >> day;
+
+  switch (day)
+  {
+  case 1:
+    cout << "Monday";
+    break;
+  case 2 :
+    cout << "Tuesday";
+    break;
+  case 3 :
+    cout << "wednesday";
+    break;
+  case 4 :
+    cout << "Thursday";
+    break;
+  case 5 :
+    cout << "Friday";
+    break;
+  case 6 :
+    cout << "Saturday";
+    break;
+  case 7 :
+    cout << "Sunday";
+    break;
+  default:
+    cout << "Invalid case";
+    break;
+  }
+}
